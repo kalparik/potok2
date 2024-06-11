d5052cc72d3ed5afbce93a58c23591619c0f483a
kalparik <63460939+kalparik@users.noreply.github.com>
2024-06-11 13:37:13 +0300

    Stub and tests for findByNumber

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,15 +1,30 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PhoneBookTest {
 
     PhoneBook phoneBook = new PhoneBook();
 
+    private final String nameExample = "Иван";
+    private final String phoneExample = "8-900-011-33-55";
+
+    @BeforeEach
+    void setUp() {
+        phoneBook.add(nameExample, phoneExample);
+    }
+
     @Test
     void add() {
-        int act = phoneBook.add("Иван", "8900-011-33-55");
+        int act = phoneBook.add(nameExample, phoneExample);
         Assertions.assertEquals(1, act);
+    }
+
+    @Test
+    void findByNumber() {
+        String name = phoneBook.findByNumber(phoneExample);
+        Assertions.assertEquals(nameExample, name);
     }
 }
